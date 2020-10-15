00b9673b2c62f3a0f0cf7988f7c94e0f150c7744
Pedro Lamas <pedrolamas@gmail.com>
2020-10-15 10:42:43 +0100

    Full code

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,90 @@
 import * as core from '@actions/core'
 import * as glob from '@actions/glob'
-// import fs from 'fs/promises'
-// import handlebars from 'handlebars'
+import fs from 'fs/promises'
+import path from 'path'
+import handlebars from 'handlebars'
 
 const run = async (): Promise<void> => {
   try {
-    const globber = await glob.create(core.getInput('files'))
-
-    for await (const file of globber.globGenerator()) {
-      core.debug(`Process: ${file}`)
-
-      // const fileContent = await fs.readFile(file)
-
-      // const template = handlebars.compile(fileContent)
-
-      // const data = {
-      //   file: {
-      //     name: file
-      //   },
-      //   env: {...process.env},
-      //   data: null
-      // }
-
-      // const newFileContent = template(data)
-
-      // fs.writeFile(file, newFileContent)
+    const config = {
+      files: core.getInput('files'),
+      dryRun: core.getInput('dry-run') === 'true',
+      outputFilename: core.getInput('output-filename')
     }
 
-    // core.setOutput('time', new Date().toTimeString())
+    core.debug(`Configuration:\n${JSON.stringify(config, undefined, 2)}`)
+
+    const outputFilenameTemplate = handlebars.compile(config.outputFilename)
+
+    const baseData = {
+      env: {...process.env}
+    }
+
+    const globber = await glob.create(config.files)
+
+    for await (const filename of globber.globGenerator()) {
+      const fileStats = await fs.stat(filename)
+
+      if (!fileStats.isFile) {
+        continue
+      }
+
+      core.debug(`Processing file "${filename}"...`)
+
+      const fileContent = await fs.readFile(filename)
+
+      core.debug(`\tCreating template...`)
+
+      const fileContentTemplate = handlebars.compile(fileContent)
+
+      core.debug(`\tApplying template...`)
+
+      const data = {
+        ...baseData,
+        file: {
+          ...path.parse(filename),
+          path: filename
+          // ...stringPropertiesOnly(fileStats)
+        },
+        date: {
+          now: new Date().toISOString()
+        }
+      }
+      const newFilename = outputFilenameTemplate(data)
+
+      const dataWithOutputFile = {
+        ...data,
+        outputFile: {
+          ...path.parse(newFilename),
+          path: newFilename
+        }
+      }
+      const newFileContent = fileContentTemplate(dataWithOutputFile)
+
+      core.debug(`\tSaving file "${newFilename}"...`)
+
+      if (!config.dryRun) {
+        await fs.writeFile(newFilename, newFileContent)
+      }
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
 }
+
+// type ObjectOfStrings = {
+//   [key: string]: string | undefined
+// }
+
+// const stringPropertiesOnly = (item: object): ObjectOfStrings => {
+//   return Object.entries(item)
+//     .filter(
+//       ([, value]) => typeof value === 'string' || typeof value === 'number'
+//     )
+//     .reduce(
+//       (res, [key, value]) => (res[key] = value.toString()),
+//       {} as ObjectOfStrings
+//     )
+// }
 
 run()
